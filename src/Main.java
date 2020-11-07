8773e600e0391e776d4f6e7f4a324fc891e45523
mohamedbabana <44004963+mohamedbabana@users.noreply.github.com>
2020-11-07 14:59:08 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,73 @@
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.MutableNode;
+
+import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.mutNode;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        /*
-         * BTree bT = new BTree(5); 
-         * BTree.Node root = bT.rootNode;
-         * root.addEntry(1,null); 
-         * System.out.println(root); 
-         * root.addEntry(0,null);
-         * System.out.println(root); 
-         * root.addEntry(4,null); 
-         * System.out.println(root);
-         * root.addEntry(3,null); 
-         * System.out.println(root); 
-         * root.removeEntry(1);
-         * System.out.println(root); 
-         * root.removeEntry(3); 
-         * System.out.println(root);
-         */
+        BTree bTree = random_tree(400,7);
+        System.setProperty("java.awt.headless", "false");
 
-         
-        BTree bTree = test_insertion();
-        // display the BTree after insertion
-        display(bTree.rootNode);
-        System.out.println("-------------");
+//        BTree bTree = test_insertion(5);
+        String rel_path = graphviz_display(bTree.rootNode, null, null, "1. Build tree");
 
-        BTree.Entry entries[] = test_get(bTree);
-
-        for(int i=0; i<entries.length; i++)
-            System.out.println(entries[i]);
+        JFrame f = new JFrame("1. Build tree");
+        ImageIcon i = new ImageIcon(rel_path);
+        f.setSize(i.getIconWidth(), i.getIconHeight() + 50);
+        f.add(new JPanel().add(new JLabel(i)));
+        f.setLocationRelativeTo(null);
+        f.show();
 
 
+        int ops_counter = 1;
+
+        while (true) {
+            System.out.println("Insert or delete a node (example : 'delete 15', 'insert 45):\n");
+            Scanner sc = new Scanner(System.in);
+            String user_command = sc.nextLine();
+            String[] user_command_array = user_command.split("\\s+");
+            boolean valid_user_command = true;
+
+            if (user_command_array.length == 2) {
+                int key = Integer.parseInt(user_command_array[1]);
+                if (user_command_array[0].equals("delete")) {
+                    bTree.single_executioner.delete(new int[]{key});
+                    rel_path = graphviz_display(bTree.rootNode, null, null, ops_counter + ". " + user_command);
+                    ops_counter++;
+                } else if (user_command_array[0].equals("insert")) {
+                    bTree.single_executioner.insert(new BTree.Entry[]{bTree.new Entry(key, key)});
+                    rel_path = graphviz_display(bTree.rootNode, null, null, ops_counter + ". " + user_command);
+                    ops_counter++;
+                } else {
+                    System.out.println("error");
+                    valid_user_command = false;
+                }
+
+                if(valid_user_command){
+                    f = new JFrame(ops_counter + ". " + user_command);
+                    i = new ImageIcon(rel_path);
+                    f.setSize(i.getIconWidth(), i.getIconHeight() + 50);
+                    f.add(new JPanel().add(new JLabel(i)));
+                    f.setLocationRelativeTo(null);
+                    f.show();
+                }
+            }
+        }
     }
 
     public BTree read_toy_example() throws IOException {
@@ -63,12 +96,11 @@ public class Main {
             int key = Integer.parseInt(line[i]);
             entries[i] = bT.new Entry(key, key);
         }
-
+        //sExecutioner.single_insert(bT.new Entry(93, 93));
         sExecutioner.insert(entries);
         
         return bT;
     }
-
     public static BTree.Entry[] test_get(BTree bt) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("data.txt"));
         String line[] = br.readLine().split("\\s+");
@@ -93,12 +125,66 @@ public class Main {
         
         System.out.println(root);
             
-        if(root.isLeaf)
+        if(root.isLeaf){
             return;
-
+        } 
+        System.out.println("\nThis node has "+ (root.used_slots+1) + " children, which are :\n{");
         for(int i=0;i<root.used_slots+1;i++)
                 if(root.childrenNode[i] != null)
                     display(root.childrenNode[i]);
+                    System.out.println("}");
         
+    }
+
+
+    public static void displaySiblings(BTree.Node root)
+    {
+        if(root==null)
+            return;
+        
+        System.out.println(root+":");
+        System.out.println(root.leftSibling+ " , ");
+        System.out.println(root.rightSibling+"\n-------------------");
+        
+        if(root.isLeaf)
+            return;
+
+        for(int i=0;i<root.used_slots+1; i++)
+                if(root.childrenNode[i] != null)
+                    displaySiblings(root.childrenNode[i]);
+        
+    }
+    public static BTree random_tree(int n_nodes, int tree_order) {
+        BTree bT = new BTree(tree_order);
+        Random random = new Random();
+        int[] array = random.ints(n_nodes, 0, n_nodes).toArray();
+        BTree.Entry[] entries_array = new BTree.Entry[n_nodes];
+        for (int i = 0; i < n_nodes; i++) {
+            entries_array[i] = bT.new Entry(array[i], array[i]);
+        }
+        bT.single_executioner.insert(entries_array);
+        return bT;
+    }
+
+    public static String graphviz_display(BTree.Node current, MutableNode graphviz_current, List<MutableNode> nodes, String tree_name) throws IOException {
+        if (nodes == null)
+            nodes = new ArrayList<>();
+        graphviz_current = mutNode(current.getEntriesString());
+        nodes.add(graphviz_current);
+
+        for (int i = 0; i < current.used_slots + 1; i++) {
+            System.out.println(current.childrenNode[i].getEntriesString());
+            MutableNode child = mutNode(current.childrenNode[i].getEntriesString());
+            graphviz_current.addLink(child);
+            if (!current.childrenNode[i].isLeaf) {
+                graphviz_display(current.childrenNode[i], child, nodes, tree_name);
+            }
+            nodes.add(child);
+        }
+
+        Graph g = graph(tree_name).directed().with(nodes);
+        String rel_path = "example/" + tree_name + ".png";
+        Graphviz.fromGraph(g).scale(1.5).render(Format.PNG).toFile(new File(rel_path));
+        return rel_path;
     }
 }
