0fb0a0a841d0903cd75a6c1fd3289bca8417efc2
Didoupower <matthieulormeau@gmail.com>
2020-11-09 23:35:06 +0100

    final commit

--- src/main.java
+++ src/main.java
@@ -9,12 +9,17 @@ public class main {
         ArrayList<int[]> r;
         t = new PerformanceTest("./generated_data.csv", 100, 12);
 
+        System.out.println("Large search queries test");
         t.test_search(0, 1000000, 20000, 5);
+        System.out.println("Small search queries test");
         t.test_search(0, 8000, 50, 10);
 
+        System.out.println("Large insert queries test");
         t.test_insert(0, 100000, 20000, 5);
+        System.out.println("Small insert queries test");
         t.test_insert(0, 8000, 50, 10);
 
+        System.out.println("Small delete queries test");
         t.test_delete(0, 8000, 500, 1);
 
 
